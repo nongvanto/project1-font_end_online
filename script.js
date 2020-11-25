b49f4a6a1eb63b04c356ab54606bda01da7b9b31
nongvanto <nongto477@gmail.com>
2020-11-25 16:20:21 +0700

    to code

--- script.js
+++ script.js
@@ -8,9 +8,52 @@ document.addEventListener('DOMContentLoaded',()=> {
     var body = document.querySelector('.body');
     var modalBtn = document.querySelectorAll('.modal-btn')
     var modalClose = document.querySelectorAll('.modal-close');
+    var btnSend = document.querySelector('.contact_form-btn')
+    var listItem = document.querySelectorAll('.list_item')
+    var scroll = document.querySelectorAll('.scroll')
+    var ft = document.querySelector('.footer')
+    console.log(ft.offsetTop)
+    for(let i=0; i<listItem.length; i++) {
+       listItem[i].addEventListener('click', ()=> {
+            for ( let k =0 ; k <listItem.length;k++) {
+                listItem[k].classList.remove('background-btn')
+            }
+           window.scrollTo(0,scroll[i].offsetTop)
+           listItem[i].classList.add('background-btn')
+           console.log(window.pageYOffset)
+       })
+    }
+    
+
     benner = parseInt(banner);
     var scrollStatus = "duoi" ;
+        window.addEventListener('scroll', () => {
+            if (window.pageYOffset>818 & window.pageYOffset<1625) {
+                for (let k =0 ; k<listItem.length; k++) {
+                    listItem[k].classList.remove('background-btn')
+                }
+                listItem[0].classList.add('background-btn')
+            } else if (window.pageYOffset>818 & window.pageYOffset<2269) {
+                for (let k =0 ; k<listItem.length; k++) {
+                    listItem[k].classList.remove('background-btn')
+                }
+                listItem[1].classList.add('background-btn')
+
+            } else if (window.pageYOffset>2269 & window.pageYOffset<3289) { 
+                for (let k =0 ; k<listItem.length; k++) {
+                    listItem[k].classList.remove('background-btn')
+                }
+                listItem[2].classList.add('background-btn')
+            } else  {
+                for (let k =0 ; k<listItem.length; k++) {
+                    listItem[k].classList.remove('background-btn')
+                }
+            }
+
+            }
+        )
     window.addEventListener('scroll', ()=> {
+        
         if (window.pageYOffset >= banner) {
             if (scrollStatus=="duoi")
             {
@@ -45,7 +88,7 @@ document.addEventListener('DOMContentLoaded',()=> {
             console.log(name)
         })
 
-       
     }
+
 })
 
